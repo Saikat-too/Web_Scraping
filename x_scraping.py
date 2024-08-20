6be110ee83c02a05189d52dec140f977f99a3a06
Saikat-too <singhasaikat37@gmail.com>
2024-08-21 00:03:36 +0600

    2nd version x_scraping

--- x_scraping.py
+++ x_scraping.py
@@ -3,31 +3,39 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import urllib.request
 
+class X_Picture:
+    
+    # init 
+    
+    def __init__(self , pic_link):
+        self.pic_link = pic_link
 
-def get_X_picture():
+    def get_X_picture(self):
 
-    # file savename 
-    picture_filename = 'x_picture2.jpg'
+        # file savename 
+        picture_filename = 'x_picture2.jpg'
 
-    # open firefox browser 
-    driver = webdriver.Firefox()
+        # open firefox browser 
+        driver = webdriver.Firefox()
 
-    # open url 
-    driver.get('https://x.com/FabrizioRomano/status/1825513124918677856/photo/1')
+        # open url 
+        driver.get(self.pic_link)
 
-    # wati enough time 
-    time.sleep(20)
+        # wati enough time 
+        time.sleep(20)
 
-    # Using beautifulsoup to find the image 
+        # Using beautifulsoup to find the image 
 
-    soup = BeautifulSoup(driver.page_source , features="lxml")
-    images = soup.find_all('img')
+        soup = BeautifulSoup(driver.page_source , features="lxml")
+        images = soup.find_all('img')
 
-    images = [img['src'] for img in images if '/media/' in img['src']]
+        images = [img['src'] for img in images if '/media/' in img['src']]
 
-    for image in images : 
-        urllib.request.urlretrieve(image , picture_filename)
+        for image in images : 
+            urllib.request.urlretrieve(image , picture_filename)
         
 
 if __name__ == '__main__':
-    get_X_picture()
+    pic_link = 'https://x.com/FabrizioRomano/status/1825513124918677856/photo/1'
+    X_Pic = X_Picture(pic_link)
+    X_Pic.get_X_picture()
